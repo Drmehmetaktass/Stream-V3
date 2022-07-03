191dfe18fd092095091f889c699457085625453c
DazBot <95157942+DazRepo@users.noreply.github.com>
2022-07-04 01:45:26 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -129,7 +129,7 @@ TG_AUDIO_FILESIZE_LIMIT = int(
 )  # Remember to give value in bytes
 
 TG_VIDEO_FILESIZE_LIMIT = int(
-    getenv("TG_VIDEO_FILESIZE_LIMIT", "523239424")
+    getenv("TG_VIDEO_FILESIZE_LIMIT", "2147483648")
 )  # Remember to give value in bytes
 
 # Chceckout https://www.gbmb.org/mb-to-bytes  for converting mb to bytes
